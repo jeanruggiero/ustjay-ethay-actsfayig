412e140244224adc665b25f7804d5c89ee8de3ce
Jean Ruggiero <jeanruggiero@gmail.com>
2019-05-21 11:06:37 -0700

    Pig latin facts complete

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
-import os
-
+from flask import Flask, Response
 import requests
-from flask import Flask, send_file, Response
+import os
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 
 def get_fact():
-
-    response = requests.get("http://unkno.com")
+    response = requests.get("http://unkno.com/")
 
     soup = BeautifulSoup(response.content, "html.parser")
-    facts = soup.find_all("div", id="content")
+    return soup.find("div", id="content").getText().strip()
 
-    return facts[0].getText()
+
+def latinize(text):
+    payload = {'input_text': text}
+    response = requests.post('https://hidden-journey-62459.herokuapp.com/piglatinize/',
+                             data=payload)
+
+    return response.url
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    address = latinize(fact)
+    html = f'<a href="{address}">{address}</a>'
+    return Response(html, mimetype="text/html")
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6787))
+if __name__ == '__main__':
+    port = os.environ.get("PORT", 8080)
     app.run(host='0.0.0.0', port=port)
 
